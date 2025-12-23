6629c9eb8ee4ac0ff1656d408baf217f07981302
RainyXeon <rainyxeon@gmail.com>
2025-12-23 13:20:07 +0700

    FROMGIT: fs: add some more missing hooks

--- fs/read_write.c
+++ fs/read_write.c
@@ -582,6 +582,10 @@ SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 	struct fd f = fdget_pos(fd);
 	ssize_t ret = -EBADF;
 
+#ifdef CONFIG_KSU
+	if (unlikely(ksu_vfs_read_hook)) 
+		ksu_handle_sys_read(fd, &buf, &count);
+#endif
 	if (f.file) {
 		loff_t pos = file_pos_read(f.file);
 		ret = vfs_read(f.file, buf, count, &pos);
