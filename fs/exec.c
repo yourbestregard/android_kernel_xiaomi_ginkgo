c8fa241bd68deedc6a34443327f1f228bba231c8
蒼雨 <rainyxeon@gmail.com>
2026-03-29 13:45:37 +0000

    FROMGIT: treewide: intergrater KernelSU hook, disable reboot hook for fork testing

--- fs/exec.c
+++ fs/exec.c
@@ -1716,6 +1716,12 @@ static int exec_binprm(struct linux_binprm *bprm)
 /*
  * sys_execve() executes a new program.
  */
+#ifdef CONFIG_KSU
+__attribute__((hot))
+extern int ksu_handle_execveat(int *fd,
+ struct filename **filename_ptr,
+ void *argv, void *envp, int *flags);
+#endif
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1729,6 +1735,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
+
+#ifdef CONFIG_KSU
+ 	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+#endif
 
 	/*
 	 * We move the actual failure in case of RLIMIT_NPROC excess from
